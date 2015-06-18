61371f27dd6abab8ea871e0c6cc461b897ede967
Jim Kinsman <jakinsman@gmail.com>
2015-06-18 13:54:00 -0500

    changing chat message

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ io.on('connection', function(socket) {
         }
     });
     socket.on('chat message', function(msg) {
-        console.log('message: ' + msg);
+        console.log('message: ' + socket.nick + ': ' + msg);
         io.emit('chat message', {from: socket.nick, msg: msg});
     });
     socket.on('disconnect', function() {
