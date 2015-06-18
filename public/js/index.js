f3261eeebbdd765cee09ebe13a06deef4bfa8986
Jim A Kinsman <jakinsman@gmail.com>
2015-06-18 18:42:44 +0000

    Revert "beautify"
    This reverts commit bbda20d1c1a38287cddc0fe68a3b608302186928.

---
+++ public/js/index.js
@@ -0,0 +1,39 @@
+var appendMessage = function(msg){
+    $('.messages ul').append($('<li>').text(msg));
+};
+
+var socket = io();
+var chatCommands = new ChatCommandProcessor(socket, appendMessage);
+
+
+$('.chatbox form').submit(function(){
+	var txt = $('.chatbox input').val();
+        if (txt.indexOf('/') === 0){
+           var cmd = txt.substring(1);
+           chatCommands.process(cmd);
+        }
+        socket.emit('chat message', $('.chatbox input').val());
+        $('.chatbox input').val('');
+	return false;
+});
+socket.on('nick given', function(nick){
+        $('.memberList .me').html(nick);
+	appendMessage('You are now known as ' + nick + ', to change your nick type /nick <new-nick>');
+});
+
+socket.on('nick change accepted', function(nick){
+   $('.memberList .me').html(nick);
+   appendMessage('You are now known as ' + nick);
+});
+
+socket.on('nick change rejected', function(nick){
+   appendMessage('Nick is already taken: ' + nick);
+});
+
+ socket.on('chat message', function(msg){
+    appendMessage(msg);
+  });
+
+socket.on('server full', function(msg){
+  appendMessage('Server full, try again later');
+})
