36a373c30843bba26c96686bd2f6ddbd294827e5
Jim A Kinsman <jakinsman@gmail.com>
2015-06-18 18:24:42 +0000

    beautify

--- server.js
+++ server.js
@@ -8,50 +8,49 @@ app.use(express.static('public'));
 
 var nick_prefixes = ['sparky', 'fido', 'wannabe', 'guest', 'frizzy', 'littlejohnny'];
 
-function randomInt(low, high){
-  return Math.floor(Math.random() * (high - low + 1) + low);
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
 }
 
-function getRandomNick(){
-   return  nick_prefixes[randomInt(0, nick_prefixes.length-1)] + randomInt(1000,99999);
+function getRandomNick() {
+    return nick_prefixes[randomInt(0, nick_prefixes.length - 1)] + randomInt(1000, 99999);
 }
 
-function checkNick(nick){
-  for (var i = 0; i < sockets.length; ++i){
-	if (sockets.nick.toLowerCase() == nick.toLowerCase()){
-		return false; 
-	}
-  }
-  return false;
+function checkNick(nick) {
+    for (var i = 0; i < sockets.length; ++i) {
+        if (sockets.nick.toLowerCase() == nick.toLowerCase()) {
+            return false;
+        }
+    }
+    return false;
 }
 
-io.on('connection', function(socket){
-        console.log('a user connected');
-        var nick = getRandomNick();
-        while(checkNick(nick) == false){
-		nick = getRandomNick();
-	}
-	socket.nick = nick;
-        socket.emit('nick given', nick);
-	sockets.push(socket);
-	socket.on('nick change', function(nick){
-		if (checkNick(nick)){
-		   socket.nick = nick;
-		   socket.emit('nick change accepted', nick);
-		}else{
-		   socket.emit('nick change rejected', nick);
-		}
-	});
-	socket.on('chat message', function(msg){
-      		console.log('message: ' + msg);
-		io.emit('chat message', msg);
-        });
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    var nick = getRandomNick();
+    while (checkNick(nick) == false) {
+        nick = getRandomNick();
+    }
+    socket.nick = nick;
+    socket.emit('nick given', nick);
+    sockets.push(socket);
+    socket.on('nick change', function(nick) {
+        if (checkNick(nick)) {
+            socket.nick = nick;
+            socket.emit('nick change accepted', nick);
+        } else {
+            socket.emit('nick change rejected', nick);
+        }
+    });
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 });
 
-http.listen(8888, function(){
-	console.log('listening on *%d', 8888);
+http.listen(8888, function() {
+    console.log('listening on *%d', 8888);
 });
- 
