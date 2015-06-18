e0b8d8817c7443cd9969ca347dc7ba346a77a80f
Jim A Kinsman <jakinsman@gmail.com>
2015-06-18 18:44:22 +0000

    beautify 2nd try

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-var appendMessage = function(msg){
+var appendMessage = function(msg) {
     $('.messages ul').append($('<li>').text(msg));
 };
 
@@ -6,34 +6,35 @@ var socket = io();
 var chatCommands = new ChatCommandProcessor(socket, appendMessage);
 
 
-$('.chatbox form').submit(function(){
-	var txt = $('.chatbox input').val();
-        if (txt.indexOf('/') === 0){
-           var cmd = txt.substring(1);
-           chatCommands.process(cmd);
-        }
-        socket.emit('chat message', $('.chatbox input').val());
-        $('.chatbox input').val('');
-	return false;
+$('.chatbox form').submit(function() {
+    var txt = $('.chatbox input').val();
+    if (txt.indexOf('/') === 0) {
+        var cmd = txt.substring(1);
+        chatCommands.process(cmd);
+    }
+    socket.emit('chat message', $('.chatbox input').val());
+    $('.chatbox input').val('');
+    return false;
 });
-socket.on('nick given', function(nick){
-        $('.memberList .me').html(nick);
-	appendMessage('You are now known as ' + nick + ', to change your nick type /nick <new-nick>');
-});
-
-socket.on('nick change accepted', function(nick){
-   $('.memberList .me').html(nick);
-   appendMessage('You are now known as ' + nick);
+socket.on('nick given', function(nick) {
+    $('.memberList .me').html(nick);
+    appendMessage('You are now known as ' + nick + ', to change your nick type /nick <new-nick>');
 });
 
-socket.on('nick change rejected', function(nick){
-   appendMessage('Nick is already taken: ' + nick);
+socket.on('nick change accepted', function(nick) {
+    $('.memberList .me').html(nick);
+    appendMessage('You are now known as ' + nick);
 });
 
- socket.on('chat message', function(msg){
+socket.on('nick change rejected', function(nick) {
+    appendMessage('Nick is already taken: ' + nick);
+});
+
+socket.on('chat message', function(msg) {
     appendMessage(msg);
-  });
+});
 
-socket.on('server full', function(msg){
-  appendMessage('Server full, try again later');
+socket.on('server full', function(msg) {
+    appendMessage('Server full, try again later');
 })
+
